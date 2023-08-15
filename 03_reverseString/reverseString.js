2a9a23edf1319d1f43fbdba81b603b7755f73b15
SamuelJayDavies <samj.davies@virginmedia.com>
2023-08-15 17:02:11 +0100

    Finished a couple of exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let charArr = string.split("");
+    let reverseArr = [];
+    let j = 0;
+    for(let i=charArr.length; i>=0; i--) {
+        reverseArr[j] = charArr[i];
+        j++;
+    }
+    return reverseArr.join("");
 };
 
 // Do not edit below this line
+console.log(reverseString("Hello World"));
 module.exports = reverseString;
